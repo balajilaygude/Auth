1c4e9d4bd0ea277ba19bd893d6dc0d2b1e2d86f9
Balaji Laygude <balajiwork05@gmail.com>
2026-06-08 20:25:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,10 @@
 const express=require("express")
 const cors=require("cors")
+const mongoose=require("mongoose")
 const bodyparser=require("body-parser")
 //whatever you import and that file need .env access must import belo this
 require("dotenv").config()
 //here
-const dbConnect = require("./Config/db")
 const router = require("./Routes/Auth.routes")
 const productRouter = require("./Routes/Products.routes")
 
@@ -27,7 +27,25 @@ app.get("/",(req,res)=>{
 //     console.log("Server Started ....")
 //     dbConnect()
 // })
-dbConnect()
+let connected=false
+const mongo_url=process.env.MONGO_URL
 
+async function dbConnect(){
 
+    try {
+        await mongoose.connect(mongo_url)
+        connected=true;
+        console.log("Databse Connected ....")
+        
+    } catch (error) {
+        console.log("Database Error " , error)
+    }
+}
+app.use((req,res,next)=>{
+    if(!connected){
+        dbConnect();
+    }
+    next();
+})
+        
 module.exports = app;
