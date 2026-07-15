11f95ede5245e96a4af9f8da9b976f616d3b3c8f
Balaji Laygude <balajiwork05@gmail.com>
2026-07-15 12:09:43 +0530

    Ui Updates

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -45,15 +45,15 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-w-screen min-h-screen bg-pink-200 flex flex-col justify-center items-center">
-      <div className="lg:w-3/5 w-4/5 h-92 bg-pink-500 flex justify-center items-center flex-col rounded-2xl shadow-2xl shadow-pink-800-">
-        <h1 className="text-4xl font-semibold my-5 text-white">Welcome {logUser}</h1>
+    <div className="min-w-screen min-h-screen bg-sky-100 flex flex-col justify-center items-center">
+      <div className="lg:w-3/5 w-4/5 h-92 bg-white flex justify-center items-center flex-col rounded-2xl shadow-2xl shadow-sky-500">
+        <h1 className="text-4xl font-semibold my-5 text-sky-500">Welcome {logUser}</h1>
         <button onClick={handleLogout} 
-        className="bg-white w-3/12 mb-1 mx-auto rounded-2xl h-12 cursor-pointer">Logout</button>
-        <div className="my-5 text-white flex gap-2">
+        className="bg-sky-500 w-3/12 mb-2 mx-auto rounded-2xl px-3 py-1 text-white hover:bg-white hover:text-sky-500 border border-sky-500 h-12 cursor-pointer">Logout</button>
+        <div className="my-5 text-sky-500 flex gap-2">
           {products &&
             products?.map((item, index) => (
-              <ul key={index} className="w-28 h-28 p-2 border border-white">
+              <ul key={index} className="w-28 h-28 p-2 border border-sky-200">
                 {item.name}  :  {item.price}$
               </ul>
             ))}
